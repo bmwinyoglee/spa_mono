1247dc394aae8ca5a88a8a26073f67fdfb1d7ff2
ben10 <tyro.rrico@gmail.com>
2025-03-03 21:18:33 +0000

    Toat messages feature complete 🔔✅

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -186,13 +186,16 @@
 
             <!-- Page Content -->
             <main>
-                <div v-if="$page.props.toast.message">
-                    {{ $page.props.toast.message }}
-                </div>
-
                 <slot />
             </main>
         </div>
+        <Toast 
+            :message="$page.props.toast.message || ''"
+            :show="showingToast"
+            @hide="showingToast = false"
+            type="success"
+            position="bottom-right"
+        />
     </div>
 </template>
 
@@ -203,7 +206,14 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+import Toast from '@/Components/toast/Toast.vue';
+import { Link, usePage, router } from '@inertiajs/vue3';
 
 const showingNavigationDropdown = ref(false);
+const showingToast = ref(false);
+
+
+router.on('finish', () =>{
+    showingToast.value = !!usePage().props.toast.message
+})
 </script>
